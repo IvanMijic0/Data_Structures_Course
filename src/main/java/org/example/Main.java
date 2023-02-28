8f164f2717e78a7e169615157f2f5d35338e4db1
IvanMijic0 <ivanmijic159951@gmail.com>
2023-03-01 00:36:05 +0100

    Update

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,10 +5,13 @@ public class Main {
     public static void main(String[] args) throws IOException {
         String path = "C://Users//User//IdeaProjects//DS_firstLab//TextFiles//numbers.txt";
         System.out.println("Reading numbers...");
+
         int[] numbers = MaxInteger.readNumbers(path);
         int maxNumber = MaxInteger.findMax(numbers);
+
         System.out.println("Maximum number is: ".concat(Integer.toString(maxNumber)));
         System.out.println("Writing to file...");
+
         MaxInteger.writeToFile(path, maxNumber);
     }
 }
