f30e4f93f2aa6d2750c5494df9bc9df5fdc64806
IvanMijic0 <ivanmijic159951@gmail.com>
2023-03-01 00:32:47 +0100

    Initial commit

---
+++ src/main/java/org/example/MaxInteger.java
@@ -0,0 +1,39 @@
+package org.example;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class MaxInteger {
+    public static int[] readNumbers(String path) throws FileNotFoundException {
+        int[] numbers = new int[100];
+        File file = new File(path);
+        Scanner scanner = new Scanner(file);
+        int i = 0;
+
+        while (scanner.hasNextLine()){
+            numbers[i] = Integer.parseInt(scanner.nextLine());
+            i++;
+        }
+        scanner.close();
+        return numbers;
+    }
+
+    public static int findMax(int[] array){
+        int max = array[0];
+        for (int i = 0; i < array.length - 1; i++) {
+            if (array[i] > max)
+                max = array[i];
+        }
+        return max;
+    }
+
+    public static void writeToFile(String path, int maxNumber) throws IOException {
+        File file = new File(path);
+        FileWriter fw = new FileWriter(file);
+        fw.write(Integer.toString(maxNumber));
+        fw.close();
+    }
+}
---
+++ src/test/java/org/test_example/MaxIntegerTest.java
@@ -0,0 +1,42 @@
+package org.test_example;
+
+import org.example.MaxInteger;
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+public class MaxIntegerTest {
+
+    static int[] numbers;
+    static  final String path = "C://Users//User//IdeaProjects//DS_firstLab//TextFiles//numbers.txt";
+
+
+    @BeforeAll
+    public static void setUpBeforeClass() throws Exception {
+        numbers = MaxInteger.readNumbers(path);
+    }
+
+    @BeforeEach
+    public void setUp() {}
+
+    @AfterEach
+    public void tearDown() {}
+
+    @AfterAll
+    public static void tearDownAfterClass() throws Exception{
+        numbers = null;
+    }
+
+    @Test
+    public void testFindsCorrectMaximum() {
+        int maxNumber = MaxInteger.findMax(numbers);
+        assertEquals(998, maxNumber);
+    }
+
+    @Test
+    public void testFindsIncorrectMaximum() {
+        int maxNumber = MaxInteger.findMax(numbers);
+        assertNotEquals(13, maxNumber);
+    }
+}
